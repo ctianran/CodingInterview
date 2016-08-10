732e46c90ea254aab0913b9c5a372f890f692fbf
ctianran <tristan20177@gmail.com>
2016-08-10 11:26:35 -0700

    add IsUniqueChars.java

---
+++ Java/IsUniqueChars.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by Tianran on 8/10/2016.
+ *  Implement an algorithm to determine if a string has
+ *  all unique characters.
+ *  What if you cannot use additional data structure?
+ */
+
+public class IsUniqueChars {
+    boolean isUniqueChars(String str){
+        if (str.length() > 128){
+            return false;
+        }
+
+    }
+}
