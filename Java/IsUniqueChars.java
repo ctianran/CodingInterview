75d38037c5ec9ee26f6658960a05b2ce1ea6b290
ctianran <tristan20177@gmail.com>
2016-08-10 12:48:02 -0700

    Add files via upload

--- Java/IsUniqueChars.java
+++ Java/IsUniqueChars.java
@@ -5,6 +5,8 @@ package com.company;
  *  Implement an algorithm to determine if a string has
  *  all unique characters.
  *  What if you cannot use additional data structure?
+ *
+ *  Time complexity : O(n)
  */
 
 public class IsUniqueChars {
@@ -12,6 +14,26 @@ public class IsUniqueChars {
         if (str.length() > 128){
             return false;
         }
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length(); i++){
+            int val = str.charAt(i);
+            if(char_set[val]){
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
 
+    boolean isUniqueCharsII(String str){
+        int checker = 0;
+        for(int i = 0; i < str.length(); i++){
+            int val = str.charAt(i) - 'a';
+            if((checker & (1 << val)) > 0){
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
     }
 }
